42642ac165742b9fa23b45c130400cd3c6d5f95b
Manoj M Wagle <74168582+manojmw@users.noreply.github.com>
2023-04-11 00:27:30 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,34 @@
 // 9 Apr 2023
 
 // Smooth scrolling
-$('.nav-link').click(function() {
-  $('html, body').animate({
-    scrollTop: $($(this).attr('href')).offset().top
-  }, 500);
-  return false;
+$('.nav-link').click(function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
 });
 
 // Show/hide navbar
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
 };
 
 
 // Awards Section
-$(document).ready(function() {
+$(document).ready(function () {
     var awardsToShow = 10;
     var totalAwards = $('.award').length;
     $('#total-awards').text(totalAwards);
 
     function updateAwardsDisplay() {
-        $('.award').each(function(index) {
+        $('.award').each(function (index) {
             if (index < awardsToShow) {
                 $(this).show();
             } else {
@@ -47,12 +47,12 @@ $(document).ready(function() {
 
     updateAwardsDisplay();
 
-    $('#show-more').on('click', function() {
+    $('#show-more').on('click', function () {
         awardsToShow += 10;
         updateAwardsDisplay();
     });
 
-    $('#view-all').on('click', function(e) {
+    $('#view-all').on('click', function (e) {
         e.preventDefault();
         awardsToShow = totalAwards;
         updateAwardsDisplay();
@@ -60,14 +60,14 @@ $(document).ready(function() {
 });
 
 // Publications Section
-$(document).ready(function() {
+$(document).ready(function () {
     var publicationsToShow = 10;
     var totalPublications = $('.publication').length;
     $('#total-publications').text(totalPublications);
 
     function updatePublicationsDisplay() {
         var count = 0;
-        $('.publication').each(function(index) {
+        $('.publication').each(function (index) {
             if (index < publicationsToShow) {
                 $(this).show();
                 count++;
@@ -86,12 +86,12 @@ $(document).ready(function() {
 
     updatePublicationsDisplay();
 
-    $('#show-more-publications').on('click', function() {
+    $('#show-more-publications').on('click', function () {
         publicationsToShow += 10;
         updatePublicationsDisplay();
     });
 
-    $('#view-all-publications').on('click', function(e) {
+    $('#view-all-publications').on('click', function (e) {
         e.preventDefault();
         publicationsToShow = totalPublications;
         updatePublicationsDisplay();
@@ -101,7 +101,7 @@ $(document).ready(function() {
         var searchTerm = $('#search-publication').val().toLowerCase();
         var selectedType = $('#type-select').val().toLowerCase();
 
-        $('.publication').each(function() {
+        $('.publication').each(function () {
             var title = $(this).find('span').first().text().toLowerCase();
             var type = $(this).find('.pub-type').text().toLowerCase();
             var matchesSearchTerm = searchTerm === '' || title.includes(searchTerm);
@@ -115,17 +115,17 @@ $(document).ready(function() {
         });
     }
 
-    $('#search-publication, #type-select').on('input', function() {
+    $('#search-publication, #type-select').on('input', function () {
         searchPublications();
     });
 });
 
 // Contact Section
 function createEmail(user, domain) {
-  return user + '@' + domain;
+    return user + '@' + domain;
 }
 
-$(document).ready(function() {
-  $('#uni-email').text(createEmail('mwag8019', 'uni.sydney.edu.au'));
-  $('#cmri-email').text(createEmail('mwagle', 'cmri.org.au'));
+$(document).ready(function () {
+    $('#uni-email').text(createEmail('mwag8019', 'uni.sydney.edu.au'));
+    $('#cmri-email').text(createEmail('mwagle', 'cmri.org.au'));
 });
